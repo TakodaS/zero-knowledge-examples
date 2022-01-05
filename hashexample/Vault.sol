965bcb3691921999655a9a786e933dc7cd4cd379
Austen Bolitho <austen.bolitho@protonmail.com>
2022-01-05 06:35:07 -0500

    added wrapper contract Vault

---
+++ hashexample/Vault.sol
@@ -0,0 +1,17 @@
+
+pragma solidity ^0.8.0;
+
+import "./verifier.sol";
+
+contract Vault is Verifier {
+  address payable receiver;
+
+  function claim(Proof memory proof) public   {
+    bool validTx = this.verifyTx(proof);
+      require(validTx, "Proof submitted was not valid!"); //error message not printed in truffle
+      address payable sender = payable(msg.sender);
+      sender.transfer(address(this).balance);
+  }
+
+  receive() external payable { }
+}
